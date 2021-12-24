54204a79e51e1da79739fee743567bc5f7bea320
Tamás Bürger <tamasburger@Tamass-MacBook-Pro.local>
2021-12-24 19:27:12 +0100

    first commit, needs refactoring and additional features

---
+++ README.md
@@ -0,0 +1,11 @@
+# Calorie Calculator
+
+This app was made to provide a simple way to calculate the necessary calorie intake depending on gender, age, weight, height, training intensity and the goal.
+Meanwhile I had the chance get bit more familiar with Material UI.
+
+## TODO
+
+1. Refactor
+2. Work on coloring (adding a theme)
+3. Improve responsiveness
+4. Play around
---
+++ src/App.tsx
@@ -0,0 +1,55 @@
+import "./App.css";
+import CalorieForm from "./CalorieForm/CalorieForm";
+import { createTheme, Paper, responsiveFontSizes } from "@mui/material";
+import { ThemeProvider } from "@emotion/react";
+
+declare module "@mui/material/styles" {
+	interface Theme {
+		toggle: {
+			thumbOnColor: string;
+			trackOnCOlor: string;
+		};
+		selected: {
+			backgroundColor: string;
+			color: string;
+		};
+		drawerPaper: { background: string };
+	}
+	// allow configuration using `createTheme`
+	interface ThemeOptions {
+		toggle: {
+			thumbOnColor: string;
+			trackOnColor: string;
+		};
+		selected: {
+			backgroundColor: string;
+			color: string;
+		};
+		drawerPaper: { background: string };
+	}
+}
+
+let theme = createTheme({
+	toggle: {
+		thumbOnColor: "yellow",
+		trackOnColor: "red",
+	},
+	selected: {
+		backgroundColor: "yellow",
+		color: "white",
+	},
+	drawerPaper: { background: "blue" },
+});
+theme = responsiveFontSizes(theme);
+
+function App() {
+	return (
+		<ThemeProvider theme={theme}>
+			{/* <div className="App"> */}
+			<CalorieForm />
+			{/* </div> */}
+		</ThemeProvider>
+	);
+}
+
+export default App;
